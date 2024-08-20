117c54cd35610dac4b14ccae268b43e36eb51333
bw02184 <bw02184@naver.com>
2024-08-20 15:50:33 +0900

    init

---
+++ src/main/java/dev/Main.java
@@ -0,0 +1,11 @@
+package dev;
+
+import dev.service.Quiz;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new Quiz().run();
+	}
+
+}
---
+++ src/main/java/dev/service/Quiz.java
@@ -0,0 +1,8 @@
+package dev.service;
+
+import lombok.extern.slf4j.Slf4j;
+
+public class Quiz {
+	public void run() {
+	}
+}
