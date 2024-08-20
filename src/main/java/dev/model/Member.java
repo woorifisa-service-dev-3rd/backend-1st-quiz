08beb9bd575ee8d725d1c3292aab3962f1c559bb
bw02184 <bw02184@naver.com>
2024-08-20 17:44:34 +0900

    Entity init

---
+++ src/main/java/dev/model/Member.java
@@ -0,0 +1,8 @@
+package dev.model;
+
+public class Member {
+	private int id;
+	private String name;
+	private String password;
+	private String ban;
+}
---
+++ src/main/java/dev/model/Test.java
@@ -0,0 +1,13 @@
+package dev.model;
+
+public class Test {
+	private int id;
+	private String subject;
+	private String type;
+	private String question;
+	private String answer;
+	private String option_1;
+	private String option_2;
+	private String option_3;
+	private String option_4;
+}
